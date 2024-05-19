fde15d3b0d6f1be861df54bca99c3147208a12e1
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-05-19 18:09:20 +0530

    Create 3068. Find the Maximum Sum of Node Values.cpp

---
+++ 3068. Find the Maximum Sum of Node Values.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    typedef long long ll;
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        ll sum = 0;
+        int cnt = 0;
+        int mini = INT_MAX;
+        for(ll num : nums){
+            if((num^k) > num){
+                sum+=(num^k);
+                cnt++;
+            } else{
+                sum+=num;
+            }
+            mini = min((long long)mini, abs(num - (num^k)));
+        }
+        if(cnt%2 == 0){
+            return sum;
+        } 
+        return sum - mini;
+    }
+};
