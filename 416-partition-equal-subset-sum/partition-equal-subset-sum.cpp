850caebcdab0e0231a47e3b6038afc7c602a7129
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-08-24 12:09:30 +0530

    Time: 96 ms (80.16%) | Memory: 99 MB (37.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,7 +8,7 @@ public:
         if(nums[0]<=k) dp[0][nums[0]] = 1;
         for(int i = 0; i<n; i++) dp[i][0] = 1;
         for(int i = 1; i<n; i++){
-            for(int j = 1; j<=k; j++){
+            for(int j = nums[0]; j<=k; j++){
                 dp[i][j] = dp[i-1][j];
                 if(nums[i]<=j) dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
             }
