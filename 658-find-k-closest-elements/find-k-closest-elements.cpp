b69604469124b450b93bc9d818b9c31f10a4d097
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-09-22 18:33:29 +0530

    Time: 27 ms (76.28%) | Memory: 33.7 MB (99.30%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<int, vector<int>, greater<int>>pq;
         int n = arr.size();
-        for(int i = 0; i<k; i++){
-            pq.push(arr[i]);
+        int l = 0, r = n-1;
+        while(r-l>=k){
+            if(abs(arr[l]-x) <= abs(arr[r]-x)) r--;
+            else l++;
         }
-        for(int i = k; i<n; i++){
-            int curr = pq.top();
-            if(abs(curr-x)> abs(arr[i]-x)){
-                pq.pop();
-                pq.push(arr[i]);
-            }
-        }
-        vector<int>ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top());
-            pq.pop();
-        }
+        vector<int>ans(arr.begin()+l, arr.begin()+l+k);
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         priority_queue<int, vector<int>, greater<int>>pq;
+//         int n = arr.size();
+//         for(int i = 0; i<k; i++){
+//             pq.push(arr[i]);
+//         }
+//         for(int i = k; i<n; i++){
+//             int curr = pq.top();
+//             if(abs(curr-x)> abs(arr[i]-x)){
+//                 pq.pop();
+//                 pq.push([i]);
+//             }
+//         }
+//         vector<int>ans;
+//         while(!pq.empty()){
+//             ans.push_back(pq.top());
+//             pq.pop();
+//         }
+//         return ans;
+//     }
+// }; TC nearly O(nlogk)
