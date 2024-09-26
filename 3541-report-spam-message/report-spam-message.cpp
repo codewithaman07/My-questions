3becea9b700a2ccdeb1babcdc0717c3cfbabffe1
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-09-26 23:59:43 +0530

    Time: 587 ms (42.21%) | Memory: 288.7 MB (57.02%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_set<string>st;
+        for(auto i : bannedWords){
+            st.insert(i);
+        }
+        int cnt = 0;
+        for(auto i : message){
+            if(st.count(i)) cnt++;
+            if(cnt>=2) return 1;
+        }
+        return 0;
+    }
+};
