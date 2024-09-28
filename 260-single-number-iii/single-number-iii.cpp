0676455ab71056363c92aa7849b20af966c4944c
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-09-28 16:31:19 +0530

    Time: 11 ms (20.10%) | Memory: 12.7 MB (41.74%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -4,7 +4,7 @@ public:
         long long  xorr = 0, n = nums.size();
         for(int i = 0; i<n; i++) xorr^=nums[i];
         int b1 = 0, b2 = 0;
-        long long  rightSetBit = (xorr&(xorr-1))^xorr;
+        long long  rightSetBit = (xorr&(xorr-1))^xorr; // we can also use xorr & (-xorr);
         for(int i = 0; i<n; i++){
             if((nums[i]*1ll) & rightSetBit) b1^=nums[i];
             else b2^=nums[i];
@@ -12,3 +12,20 @@ public:
         return {b1,b2};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> singleNumber(vector<int>& nums) {
+//         unordered_map<int,int>mpp;
+//         for(auto x : nums){
+//             mpp[x]++;
+//         }
+//         vector<int>ans;
+//         for(auto x : mpp){
+//             if(x.second == 1){
+//                 ans.push_back(x.first);
+//             }
+//         }
+//         return ans;
+//     }
+// };
