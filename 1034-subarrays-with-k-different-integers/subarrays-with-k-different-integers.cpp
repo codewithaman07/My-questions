17292eadaa8c1c40c10859400589cd86dbd54da3
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-09-30 17:09:26 +0530

    Time: 44 ms (96.62%) | Memory: 39.9 MB (99.74%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int k){
-        int l = 0, r = 0, n = nums.size(), cnt = 0;
-        map<int,int>mp;
-        while(r<n){
-            mp[nums[r]]++;
-            while(mp.size()>k){
-                if(--mp[nums[l]] == 0) mp.erase(nums[l]);
-                l++;
-            }
-            cnt+=(r-l+1);
-            r++;
-        }
-        return cnt;
-    }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return solve(nums,k)- solve(nums, k-1);
+    int cnt[20001] = {}, res = 0, sz = nums.size();
+    for (int l = 0, m = 0, r = 0; r < sz; ++r) {
+        if (++cnt[nums[r]] == 1)
+            if (--k < 0) {
+                cnt[nums[m++]] = 0;
+                l = m;
+            }
+        if (k <= 0) {
+            while (cnt[nums[m]] > 1)
+                --cnt[nums[m++]];
+            res += m - l + 1;   
+        }
     }
+    return res;
+    } 
+
+
+
+
+
+
+    
+    // int solve(vector<int>&nums, int k){
+    //     int l = 0, r = 0, n = nums.size(), cnt = 0;
+    //     map<int,int>mp;
+    //     while(r<n){
+    //         mp[nums[r]]++;
+    //         while(mp.size()>k){
+    //             if(--mp[nums[l]] == 0) mp.erase(nums[l]);
+    //             l++;
+    //         }
+    //         cnt+=(r-l+1);
+    //         r++;
+    //     }
+    //     return cnt;
+    // }
+    // int subarraysWithKDistinct(vector<int>& nums, int k) {
+    //     return solve(nums,k)- solve(nums, k-1);
+    // }
 };
