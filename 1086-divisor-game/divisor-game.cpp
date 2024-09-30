024b2b9373c664e3d85ee151d7046100d696f785
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-09-30 23:01:32 +0530

    Time: 3 ms (25.90%) | Memory: 7.9 MB (6.61%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -4,7 +4,7 @@ public:
         if(n == 1) return 0;
         if(dp[n] != -1) return dp[n];
         bool ans = 0;
-        for(int i = 1; i*i<n; i++){
+        for(int i = 1; i*i<=n; i++){
             if(n % i == 0){
                 if(!solve(n-i, dp)){
                     ans = 1;
