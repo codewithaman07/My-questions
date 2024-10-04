6a16bb5aec2282e2a98f387bb8b4a75826b79524
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-10-04 09:11:21 +0530

    Time: 54 ms (92.60%) | Memory: 55.8 MB (98.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(), skill.end());
-        int n = skill.size(), sum = skill[0]+skill[n-1];
-        int l = 0, r = n-1;
+        int n = skill.size();
+        int n2 = n/2;
+        vector<int>cnt(1001,0);
+        int sum = 0;
         long long ans = 0;
-        while(l<r){
-            if(sum == skill[l]+skill[r]){
-                ans+=(skill[l]*skill[r]);
-                l++; r--;
-            }
-            else return -1;
+        for(int i = 0; i<n; i++){
+            sum+=skill[i];
+            cnt[skill[i]]++;
         }
-        return ans;
+        if(sum%n2 != 0) return -1;
+        int x = sum/n2;
+        for(int i = 0; i<n; i++){
+            int a = skill[i];
+            int b = x-a;
+            if(cnt[b]<=0) return -1;
+            ans+=(a*b);
+            cnt[b]--;
+        }
+        return ans/2;
     }
 };
+
+// class Solution {
+// public:
+//     long long dividePlayers(vector<int>& skill) {
+//         sort(skill.begin(), skill.end());
+//         int n = skill.size(), sum = skill[0]+skill[n-1];
+//         int l = 0, r = n-1;
+//         long long ans = 0;
+//         while(l<r){
+//             if(sum == skill[l]+skill[r]){
+//                 ans+=(skill[l]*skill[r]);
+//                 l++; r--;
+//             }
+//             else return -1;
+//         }
+//         return ans;
+//     }
+// };
