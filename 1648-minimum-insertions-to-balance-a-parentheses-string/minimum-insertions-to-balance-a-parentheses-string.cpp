5e43512d3beb9775cbd063b349d82773f126b6a8
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-10-08 09:10:24 +0530

    Time: 27 ms (81.40%) | Memory: 13.7 MB (93.72%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        stack<char>st;
         int n = s.size();
-        int ans = 0;
+        int ans = 0, num = 0;
         for(int i = 0; i<n; i++){
-            if(s[i] == '(') st.push(s[i]);
+            if(s[i] == '(') num++;
             else{
                 if(i+1< n && s[i+1] == ')'){
-                    if(st.empty()) ans++;
-                    else st.pop();
+                    if(num == 0) ans++;
+                    else num--;
                     i++;
                 }
                 else{
-                    if(st.empty()) ans+=2;
-                    else ans++, st.pop();
+                    if(num == 0) ans+=2;
+                    else ans++, num--;
                 }
             }
         }
-        if(!st.empty()){
-            ans+=(st.size()*2);
+        if(num!=0){
+            ans+=(num*2);
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int minInsertions(string s) {
+//         stack<char>st;
+//         int n = s.size();
+//         int ans = 0;
+//         for(int i = 0; i<n; i++){
+//             if(s[i] == '(') st.push(s[i]);
+//             else{
+//                 if(i+1< n && s[i+1] == ')'){
+//                     if(st.empty()) ans++;
+//                     else st.pop();
+//                     i++;
+//                 }
+//                 else{
+//                     if(st.empty()) ans+=2;
+//                     else ans++, st.pop();
+//                 }
+//             }
+//         }
+//         if(!st.empty()){
+//             ans+=(st.size()*2);
+//         }
+//         return ans;
+//     }
+// };
