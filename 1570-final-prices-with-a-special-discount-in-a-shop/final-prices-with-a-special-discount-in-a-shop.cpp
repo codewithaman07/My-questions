4764b84fd0c8dbd5efee63555ec1e1f040ca3aa7
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-10-10 09:49:54 +0530

    Time: 4 ms (53.67%) | Memory: 12.7 MB (61.31%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int>st;
+        int n = prices.size();
+        vector<int>nse(n,-1);
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && prices[st.top()]>prices[i]) st.pop();
+            if(!st.empty()) nse[i] = prices[st.top()];
+            st.push(i);
+        }
+        for(int i = 0; i<n; i++){
+            if(nse[i] == -1) nse[i] = prices[i];
+            else nse[i] = prices[i]-nse[i];
+        }
+        return nse;
+    }
+};
