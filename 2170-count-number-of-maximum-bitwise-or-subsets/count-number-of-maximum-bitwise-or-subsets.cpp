99c65de1e41ca147565f83c187903be4d30603da
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-10-18 11:01:12 +0530

    Time: 114 ms (12.48%) | Memory: 309.1 MB (9.56%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int i, int n, int curr, int maxi){
+    int solve(vector<int>&nums, int i, int n, int curr, int maxi, vector<vector<int>>&dp){
         if(i == n) return curr == maxi ? 1 : 0;
-        int x = solve(nums, i+1, n, curr, maxi);
-        int y = solve(nums, i+1, n, curr|nums[i], maxi);
-        return x+y;
+        if(dp[i][curr] != -1) return dp[i][curr];
+        int x = solve(nums, i+1, n, curr, maxi,dp);
+        int y = solve(nums, i+1, n, curr|nums[i], maxi,dp);
+        return dp[i][curr] = x+y;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int ans = 0, maxi = 0, n = nums.size();
         for(int i = 0; i<n; i++) maxi |= nums[i];
-        return solve(nums, 0, n, 0, maxi);
+        vector<vector<int>>dp(n, vector<int>(maxi+1, -1));
+        return solve(nums, 0, n, 0, maxi, dp);
     }
 };
