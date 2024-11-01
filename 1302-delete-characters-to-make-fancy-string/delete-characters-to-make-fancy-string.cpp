dd97d840e83293f67098b8d177345a68b61a0f4e
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-11-01 10:39:27 +0530

    Time: 29 ms (45.45%) | Memory: 42.8 MB (30.23%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        int n = s.size(), j = 2;
-        if(n<3) return s;
-        for(int i = 2; i<n; i++){
-            if(s[i] != s[j-1] || s[i] != s[j-2]){
-                s[j++] = s[i];
+        int n = s.size(), freq = 1;
+        char prev = s[0];
+        string ans;
+        ans+=s[0];
+        for(int i = 1; i<n; i++){
+            if(s[i] == prev) freq++;
+            else{
+                prev = s[i];
+                freq = 1;
             }
+            if(freq<3) ans+=s[i];
         }
-        s.resize(j);
-        return s;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     string makeFancyString(string s) {
+//         int n = s.size(), j = 2;
+//         if(n<3) return s;
+//         for(int i = 2; i<n; i++){
+//             if(s[i] != s[j-1] || s[i] != s[j-2]){
+//                 s[j++] = s[i];
+//             }
+//         }
+//         s.resize(j);
+//         return s;
+//     }
+// };
