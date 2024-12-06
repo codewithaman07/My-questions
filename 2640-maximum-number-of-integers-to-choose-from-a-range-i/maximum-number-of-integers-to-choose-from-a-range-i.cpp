b6f4d45fb2d28d82d292024743db9a54ac804904
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-12-06 19:59:27 +0530

    Time: 71 ms (81.68%) | Memory: 117.6 MB (82.66%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        unordered_set<int>st(banned.begin(), banned.end());
         int cnt = 0, sum = 0;
+        sort(banned.begin(), banned.end());
         for(int i = 1; i<=n; i++){
-            if(st.count(i)) continue;
+            if(binary_search(banned.begin(), banned.end(), i)) continue;
             if(sum+i > maxSum) break;
             sum+=i;
-            cnt++;
+            cnt++; 
         }
         return cnt;
     }
 };
+
+// int maxCount(vector<int>& banned, int n, int maxSum) {
+//         unordered_set<int>st(banned.begin(), banned.end());
+//         int cnt = 0, sum = 0;
+//         for(int i = 1; i<=n; i++){
+//             if(st.count(i)) continue;
+//             if(sum+i > maxSum) break;
+//             sum+=i;
+//             cnt++;
+//         }
+//         return cnt;
+//     }
