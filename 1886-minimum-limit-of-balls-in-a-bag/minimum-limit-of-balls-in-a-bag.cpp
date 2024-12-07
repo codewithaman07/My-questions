342d2963a7e34d3a6435f24cb2beca106825fdf3
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-12-07 18:53:19 +0530

    Time: 33 ms (61.33%) | Memory: 59.8 MB (15.54%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool fun(vector<int>&nums, int maxOperations, int mid, int n){
+        int cnt = 0;
+        for(int i = 0; i<n; i++){ // ceil (a+b-1)/b
+            cnt+= ((nums[i]+mid-1)/mid)-1;
+        }
+        return cnt <= maxOperations;
+    }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int l = 1, h = *max_element(nums.begin(), nums.end()), n = nums.size();
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(fun(nums, maxOperations, mid, n)) h = mid-1;
+            else l = mid+1;
+        }
+        return l;
+    }
+};
