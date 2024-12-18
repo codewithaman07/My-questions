49f6ae256cf143fcb95478dae1adb27c1a6edd89
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-12-18 10:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (28.27%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        stack<int>st;
         int n = prices.size();
+        stack<int>st;
         vector<int>nse(n,-1);
         for(int i = n-1; i>=0; i--){
-            while(!st.empty() && prices[st.top()]>prices[i]) st.pop();
+            while(!st.empty() && prices[st.top()] > prices[i]) st.pop();
             if(!st.empty()) nse[i] = prices[st.top()];
             st.push(i);
         }
