37aef29de14c8f5563805c7fd621cbc94e9fcab9
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-01-16 12:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 64.2 MB (82.93%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    #define ll long long
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<ll,ll>mp;
-        int n = nums1.size(), m = nums2.size(), ans = 0;
-        for(int i = 0; i<n; i++) mp[nums1[i]]+=m;
-        for(int i = 0; i<m; i++) mp[nums2[i]]+=n;
-        for(auto it : mp){
-            ll num = it.first;
-            ll freq = it.second;
-            if(freq&1ll) ans^=num;
-        }
+        int ans = 0, n = nums1.size(), m = nums2.size();
+        if(m&1) for(int num : nums1) ans^=num;
+        if(n&1) for(int num : nums2) ans^=num;
         return ans;
     }
 };
+
+//class Solution {
+// public:
+//     #define ll long long
+//     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_map<ll,ll>mp;
+//         int n = nums1.size(), m = nums2.size(), ans = 0;
+//         for(int i = 0; i<n; i++) mp[nums1[i]]+=m;
+//         for(int i = 0; i<m; i++) mp[nums2[i]]+=n;
+//         for(auto it : mp){
+//             ll num = it.first;
+//             ll freq = it.second;
+//             if(freq&1ll) ans^=num;
+//         }
+//         return ans;
+//     }
+// };
