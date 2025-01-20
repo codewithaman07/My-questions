08b07c6f0449350203cd96d0f45d50220d615516
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-01-20 20:47:58 +0530

    Time: 8 ms (87.20%) | Memory: 133.9 MB (86.89%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        unordered_map<int,int>row, col;
-        unordered_map<int,pair<int,int>>grid;
+        vector<int>row(n,0), col(m,0);
+        vector<pair<int,int>>grid(n*m+1);
         for(int i = 0; i<n; i++) for(int j = 0; j<m; j++) grid[mat[i][j]] = {i,j};
         for(int i = 0; i<arr.size(); i++){
             if(++row[grid[arr[i]].first] == m || ++col[grid[arr[i]].second] == n) return i;
