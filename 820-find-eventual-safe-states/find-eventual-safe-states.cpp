01de069303d543f8a0a313cf50a8d031d16f0e7b
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-01-24 10:44:15 +0530

    Time: 53 ms (34.65%) | Memory: 66.4 MB (15.64%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<vector<int>>adjrev(n);
-        vector<int>indegree(n,0);
+        vector<vector<int>>adj(n);
+        vector<int>degree(n,0);
         for(int i = 0; i<n; i++){
             for(auto it : graph[i]){
-                adjrev[it].push_back(i);
-                indegree[i]++;
+                adj[it].push_back(i);
+                degree[i]++;
             }
         }
         queue<int>q;
-        vector<int>safeNodes;
         for(int i = 0; i<n; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
+            if(!degree[i]) q.push(i);
         }
+        vector<int>ans;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            safeNodes.push_back(node);
-            for(auto it : adjrev[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                degree[it]--;
+                if(!degree[it]) q.push(it);
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
