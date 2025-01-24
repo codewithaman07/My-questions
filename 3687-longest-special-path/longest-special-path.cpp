18ea75cc9cd74be029b6312a67f12198d1aaf278
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-01-24 10:27:58 +0530

    Time: 1281 ms (16.28%) | Memory: 237.2 MB (69.29%) - LeetSync

--- 3687-longest-special-path/longest-special-path.cpp
+++ 3687-longest-special-path/longest-special-path.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    pair<int,int>ans;
-    vector<int>depths;
-    Solution() : depths(50001,0) {}
-
-    void dfs(int node, int parent, vector<vector<pair<int,int>>>&adj, vector<int>&nums, int maxd, int currd,vector<int>&pre){
-        int pred = exchange(depths[nums[node]], currd);
-        maxd = max(maxd, pred);
-        if (maxd < currd) {
-            ans = min(ans, {-(pre.back() - pre[maxd]), currd - maxd});
+    int maxi = 0, mini = INT_MAX;
+    void dfs(int node, int parent, vector<vector<pair<int,int>>>&adj, vector<int>&nums, int pathsum, int s, int e, unordered_map<int,int>&mp ,vector<int>&path){
+        int pred = mp.count(nums[node]) ? mp[nums[node]] : -1;
+        while(s<=pred) pathsum-= path[s++];
+        if(pathsum > maxi){
+            maxi = pathsum;
+            mini = e-s+1;
         }
-        for(auto& [neighbor, len] : adj[node]){
+        else if(pathsum == maxi) mini = min(mini, e-s+1);
+        mp[nums[node]] = e;
+
+        for(auto& [neighbor , len] : adj[node]){
             if(neighbor == parent) continue;
-            pre.push_back(pre.back()+len);
-            dfs(neighbor, node, adj, nums, maxd, currd+1, pre);
-            pre.pop_back();
+            path.push_back(len);
+            dfs(neighbor, node, adj, nums, pathsum+len, s, e+1, mp, path);
+            path.pop_back();
         }
-        depths[nums[node]] = pred;
+        mp[nums[node]] = pred;
     }
-
     vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
         int n = nums.size();
         vector<vector<pair<int,int>>>adj(n);
@@ -26,9 +26,9 @@ public:
             adj[e[0]].push_back({e[1], e[2]});
             adj[e[1]].push_back({e[0],e[2]});
         }
-        vector<int>pre{0};
-        ans = {INT_MAX, INT_MAX};
-        dfs(0,-1,adj,nums,0,1,pre);
-        return {-ans.first, ans.second};
+        vector<int>path;
+        unordered_map<int,int>mp;
+        dfs(0,-1,adj,nums,0,0,0,mp,path);
+        return {maxi, mini};
     }
 };
