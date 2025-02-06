794d8414d9490c08b10ec17f33abf373745f4e0f
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-02-06 21:08:39 +0530

    Time: 4 ms (76.88%) | Memory: 13.9 MB (83.71%) - LeetSync

---
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    #define ll long long 
+    long long appealSum(string s) {
+        int n = s.size();
+        ll ans = 0, curr = 0;
+        vector<int>mp(26,-1);
+        for(int i = 0; i<n; i++){
+            curr+= (i-mp[s[i]-'a']);
+            ans+= curr;
+            mp[s[i]-'a'] = i;
+        }
+        return ans;
+    }
+};
