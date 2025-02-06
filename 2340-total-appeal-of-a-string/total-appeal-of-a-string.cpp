adcc2137c802ae89b814f591824db619f001f659
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-02-06 21:30:11 +0530

    Time: 38 ms (25.04%) | Memory: 13.9 MB (83.71%) - LeetSync

--- 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
@@ -3,13 +3,24 @@ public:
     #define ll long long 
     long long appealSum(string s) {
         int n = s.size();
-        ll ans = 0, curr = 0;
-        vector<int>mp(26,-1);
-        for(int i = 0; i<n; i++){
-            curr+= (i-mp[s[i]-'a']);
-            ans+= curr;
-            mp[s[i]-'a'] = i;
+        ll ans = 0;
+        for(char ch = 'a'; ch<='z'; ch++){
+            int temp = 0;
+            for(int i = 0; i<n; i++){
+                if(s[i] == ch) temp = i+1;
+                ans+=temp;
+            }
         }
         return ans;
     }
 };
+
+        // int n = s.size();
+        // ll ans = 0, curr = 0;
+        // vector<int>mp(26,-1);
+        // for(int i = 0; i<n; i++){
+        //     curr+= (i-mp[s[i]-'a']);
+        //     ans+= curr;
+        //     mp[s[i]-'a'] = i;
+        // }
+        // return ans;
