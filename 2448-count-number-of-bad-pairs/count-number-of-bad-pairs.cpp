6e484e65f2eca4ca58c07b7b0a6619676d5a57a8
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-02-09 10:26:40 +0530

    Time: 65 ms (73.25%) | Memory: 92.6 MB (9.57%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     #define ll long long 
+    ll fun(ll n){
+        return (n*(n-1)/2);
+    }
     long long countBadPairs(vector<int>& nums) {
-        ll ans = 0;
-        int n = nums.size();
-        vector<int>temp;
-        for(int i = 0; i<n; i++){
-            temp.push_back(nums[i]-i);
+        ll ans = 0, n = nums.size();
+        unordered_map<ll,ll>mp;
+        for(ll i = 0; i<n; i++){
+            ll num = nums[i];
+            mp[num-i]++;
         }
-        sort(temp.begin(),temp.end());
-        for(int i = 0; i<n; i++){
-            ll t = temp[i];
-            int idx = upper_bound(temp.begin(), temp.end(), t) - temp.begin();
-            ans+=(n-idx);
+        for(auto &[x,y] : mp){
+            if(y>1) ans+=fun(y);
         }
-        return ans;
+        return fun(n)-ans;
     }
 };
+
+// class Solution {
+// public:
+//     #define ll long long 
+//     long long countBadPairs(vector<int>& nums) {
+//         ll ans = 0;
+//         int n = nums.size();
+//         vector<int>temp;
+//         for(int i = 0; i<n; i++){
+//             temp.push_back(nums[i]-i);
+//         }
+//         sort(temp.begin(),temp.end());
+//         for(int i = 0; i<n; i++){
+//             ll t = temp[i];
+//             int idx = upper_bound(temp.begin(), temp.end(), t) - temp.begin();
+//             ans+=(n-idx);
+//         }
+//         return ans;
+//     }
+// };
