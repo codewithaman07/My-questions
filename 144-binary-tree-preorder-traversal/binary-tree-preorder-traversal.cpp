9a1f8aa0a7e6693b23e264ac8b6fe58b15b96499
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-02-10 19:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (42.46%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(!root) return ans;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            auto node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
+        }
+        return ans;
+    }
+};
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void preorder(TreeNode* root, vector<int>&ans){
+//         if(!root) return;
+//         ans.push_back(root->val);
+//         preorder(root->left,ans);
+//         preorder(root->right,ans);
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         preorder(root, ans);
+//         return ans;
+//     }
+// };
