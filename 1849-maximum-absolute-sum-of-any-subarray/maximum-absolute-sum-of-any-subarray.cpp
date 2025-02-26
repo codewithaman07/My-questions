8672a5c3db292ed6b977e54aa79b906283fec45e
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-02-26 09:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    int maxAbsoluteSum(vector<int>& nums) {
-        int n = nums.size(), mini = 0, maxi = 0;
-        for(int i = 0; i<n; i++){
-            int x = nums[i];
-            mini = min(mini+x, 0);
-            maxi = max(maxi+x, 0);
+    int minSubArraySum(vector<int>& nums) {
+        int minSum = INT_MAX, currSum = 0;
+        for (int num : nums) {
+            currSum += num;
+            minSum = min(minSum, currSum);
+            if (currSum > 0) currSum = 0;
         }
-        return maxi-mini;
+        return minSum;
+    }
+    int maxSubArraySum(vector<int>& nums) {
+        int maxSum = INT_MIN, minSum = 0, currSum = 0;
+        for (int num : nums) {
+            currSum += num;
+            maxSum = max(maxSum, currSum - minSum);
+            minSum = min(minSum, currSum);
+        }
+        return maxSum;
+    }
+    int maxAbsoluteSum(vector<int>& nums) {
+        int a = maxSubArraySum(nums);
+        a = abs(a);
+        int b = minSubArraySum(nums);
+        b = abs(b);
+        return max(a,b);
     }
 };
