5f2e75d03fc789e20271cd4936ae28b3be05f441
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-03-15 09:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (43.57%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,51 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        if(root == nullptr) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<vector<int>>vec;
-        while(!q.empty()){
-            int k = q.size();
-            vector<int>level;
-            while(k--){
-                TreeNode* a = q.front();
-                q.pop();
-                level.push_back(a->val);
-                if(a->left) q.push(a->left);
-                if(a->right) q.push(a->right);
-            }
-            vec.push_back(level);
-        }
-        for(auto i : vec){
-            ans.push_back(i.back());
-        }
-        return ans;
-    }
-};
-
- 
-/*class Solution {
 public:
     void solve(TreeNode* root, vector<int>&ans, int level){
         if(!root) return;
-        if(ans.size() == level){
-            ans.push_back(root->val);
-        }
-        solve(root->right,ans,level+1);
+        if(ans.size() == level) ans.push_back(root->val);
+        solve(root->right, ans, level+1);
         solve(root->left, ans, level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-        int level = 0;
-        if(!root) return ans;
-        solve(root,ans,level);
+        solve(root, ans, 0);
         return ans;
     }
 };
-*/
