302856860a38b0107a0bd2c2b909eee2347d5e70
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-03-18 12:00:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (33.85%) - LeetSync

--- 2383-add-two-integers/add-two-integers.cpp
+++ 2383-add-two-integers/add-two-integers.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        while(num2 != 0){
-            int carry = num1&num2;
-            num1 = num1^num2;
-            num2 = carry << 1;
-        }
-        return num1;
+        if(!num2) return num1;
+        return sum(num1^num2, (num1&num2)<<1);
     }
 };
