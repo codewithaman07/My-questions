36a0fd3be01698c198f4852cab582b1a02dc5830
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-03-18 13:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (42.75%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,44 +11,15 @@
  */
 class Solution {
 public:
+    void solve(TreeNode* root, vector<int>&ans){
+        if(!root) return;
+        ans.push_back(root->val);
+        solve(root->left, ans);
+        solve(root->right, ans);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            auto node = st.top();
-            st.pop();
-            ans.push_back(node->val);
-            if(node->right) st.push(node->right);
-            if(node->left) st.push(node->left);
-        }
+        solve(root, ans);
         return ans;
     }
 };
-
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     void preorder(TreeNode* root, vector<int>&ans){
-//         if(!root) return;
-//         ans.push_back(root->val);
-//         preorder(root->left,ans);
-//         preorder(root->right,ans);
-//     }
-//     vector<int> preorderTraversal(TreeNode* root) {
-//         vector<int>ans;
-//         preorder(root, ans);
-//         return ans;
-//     }
-// };
