648cb06e7eb6c678a35b267ef17b6e16fa63322f
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-03-21 00:29:33 +0530

    Time: 15 ms (83.44%) | Memory: 208 MB (63.96%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -9,33 +9,25 @@ public:
         parent[y] = x;
     }
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        vector<int>cost(n,-1);
         parent.resize(n);
-        vector<int>cost(n);
-        for(int i = 0; i<n; i++){
-            parent[i] = i;
-            cost[i] = -1;
-        }
+        for(int i = 0; i<n; i++) parent[i] = i;
         for(auto &e : edges){
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-            int pu = find(u);
-            int pv = find(v);
+            int u = e[0], v = e[1], w = e[2];
+            int pu = find(u), pv = find(v);
             if(pu != pv){
-                Union(pu, pv);
+                Union(pu,pv);
                 cost[pu] &= cost[pv];
             }
             cost[pu]&=w;
         }
         vector<int>ans;
-        for(auto q : query){
-            int a = q[0];
-            int b = q[1];
-            int pa = find(a);
-            int pb = find(b);
-            if(a == b) ans.push_back(0);
-            else if(pa != pb) ans.push_back(-1);
-            else ans.push_back(cost[parent[a]]);
+        for(auto &q : query){
+            int u = q[0], v = q[1];
+            int pu = find(u), pv = find(v);
+            if(u == v) ans.push_back(0);
+            else if(pu != pv) ans.push_back(-1);
+            else ans.push_back(cost[parent[u]]);
         }
         return ans;
     }
