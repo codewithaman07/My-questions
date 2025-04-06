4a5cba8ebb52c5d5a7c45730a0371ae2c42051ab
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-04-06 11:41:11 +0530

    Time: 15 ms (31.03%) | Memory: 12.5 MB (73.14%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,32 +1,75 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int>dp(n,1), hash(n,1);
-        for(int i = 0; i<n; i++){
-            hash[i] = i;
-            for(int prev = 0; prev<i; prev++){
-                if(nums[i]%nums[prev] == 0 && dp[i] < 1+dp[prev]){
-                    dp[i] = 1+dp[prev];
-                    hash[i] = prev;
+        int n = nums.size(), last = 0, maxi = 1;
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n,1), prev(n,-1);
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(nums[i]%nums[j] == 0 && dp[i]<dp[j]+1){
+                    dp[i] = 1+dp[j];
+                    prev[i] = j;
+                }
+                if(dp[i]>maxi){
+                    maxi = dp[i];
+                    last = i;
                 }
             }
         }
-        int ans = -1, last = -1;
-        for(int i = 0; i<n; i++){
-            if(dp[i] > ans){
-                ans = dp[i];
-                last = i;
-            }
+        vector<int>ans;
+        while(last != -1){
+            ans.push_back(nums[last]);
+            last = prev[last];
         }
-        vector<int>temp;
-        temp.push_back(nums[last]);
-        while(last != hash[last]){
-            last = hash[last];
-            temp.push_back(nums[last]);
-        }
-        reverse(temp.begin(), temp.end());
-        return temp;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     void solve(int idx, int prev, vector<int>&temp, vector<int>&nums, vector<int>&ans){
+//         int n = nums.size();
+//         if(idx>=n){
+//             if(temp.size()>ans.size()) ans = temp;
+//             return;
+//         }
+//         if(prev == -1 || nums[idx]%prev == 0){
+//             temp.push_back(nums[idx]);
+//             solve(idx+1,nums[idx], temp,nums,ans);
+//             temp.pop_back();
+//         }
+//         solve(idx+1,prev, temp,nums,ans);
+//     }
+//     vector<int> largestDivisibleSubset(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<int>temp, ans;
+//         int prev = -1;
+//         solve(0,prev,temp,nums,ans);
+//         return ans;
+//     }
+// };
+// class Solution {
+// public:
+//     void solve(int idx, vector<int>&temp, vector<int>&nums, vector<int>&ans){
+//         int n = nums.size();
+//         if(idx>=n){
+//             if(temp.size()>ans.size()) ans = temp;
+//             return;
+//         }
+//         for(int i = idx; i<n; i++){
+//             bool f = 0;
+//             if(temp.size() == 0)temp.push_back(nums[i]), f = 1;
+//             else{
+//                 if(nums[i]%temp.back() == 0) temp.push_back(nums[i]), f = 1;
+//             }
+//             solve(i+1, temp,nums,ans);
+//             if(f) temp.pop_back();
+//         }
+//     }
+//     vector<int> largestDivisibleSubset(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<int>temp, ans;
+//         solve(0,temp,nums,ans);
+//         return ans;
+//     }
+// };
