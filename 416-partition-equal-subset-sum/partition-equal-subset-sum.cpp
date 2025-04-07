7464d0df3a629efb0ce9cac6c0b9b34aea19e19e
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-04-07 13:44:59 +0530

    Time: 63 ms (86.66%) | Memory: 101.1 MB (21.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    bool solve(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
-        if(j == 0) return 1;
-        if(i<0) return 0;
-        if(i == 0 && j == nums[0]) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        bool np = solve(i-1, j, nums, dp);
-        bool p = 0;
-        if(nums[i]<=j){
-            p = solve(i-1, j-nums[i], nums, dp);
-        }
-        return dp[i][j] = p || np;
+    bool solve(int i, int sum, int t, vector<int>&nums, vector<vector<int>>&dp){
+        int n = nums.size();
+        if(sum == t) return 1;
+        if(i>=n || sum>t) return 0;
+        if(dp[i][sum] != -1) return dp[i][sum];
+        return dp[i][sum] = (solve(i+1,sum+nums[i], t, nums, dp) || solve(i+1,sum,t,nums, dp));
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(),0);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum&1) return 0;
-        int k = sum/2, n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return solve(n-1,k,nums,dp);
-
-
-
-
-
-
-        // if(nums[0]<=k) dp[0][nums[0]] = 1;
-        // for(int i = 0; i<n; i++) dp[i][0] = 1;
-        // for(int i = 1; i<n; i++){
-        //     for(int j = nums[0]; j<=k; j++){
-        //         dp[i][j] = dp[i-1][j];
-        //         if(nums[i]<=j) dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
-        //     }
-        // }
-        // return dp[n-1][k];
+        int t = sum/2, n = nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(t+1,-1));
+        return solve(0,0,t,nums, dp);
     }
 };
+
+// class Solution {
+// public:
+//     bool solve(int i, int sum, int t, vector<int>&nums){
+//         int n = nums.size();
+//         if(sum == t) return 1;
+//         if(i>=n) return 0;
+//         return solve(i+1,sum+nums[i], t, nums) || solve(i+1,sum,t,nums);
+//     }
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(), nums.end(), 0);
+//         if(sum&1) return 0;
+//         int t = sum/2;
+//         return solve(0,0,t,nums);
+//     }
+// };
