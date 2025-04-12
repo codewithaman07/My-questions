4bd366a25fb9fc31fa74a10de39da1147257ca64
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-04-12 10:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (55.92%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int calculate(string s) {
+        int n = s.size(), ans = 0, num = 0, sign = 1;
         stack<int>st;
-        int num = 0, sign = 1, ans = 0, n = s.size();
         for(int i = 0; i<n; i++){
-            if(isdigit(s[i])) num = num*10+(s[i]-'0');
+            if(isdigit(s[i])) num = (num*10)+(s[i]-'0');
             else if(s[i] == '+'){
                 ans+=(num*sign);
-                num = 0; sign = 1;
+                num = 0, sign = 1;
             }
             else if(s[i] == '-'){
                 ans+=(num*sign);
-                num = 0; sign = -1;
+                num = 0, sign = -1;
             }
             else if(s[i] == '('){
-                st.push(ans); st.push(sign);
-                ans = 0; sign = 1; num = 0;
+                st.push(ans);
+                st.push(sign);
+                num = 0, sign = 1, ans = 0;
             }
             else if(s[i] == ')'){
                 ans+=(sign*num);
