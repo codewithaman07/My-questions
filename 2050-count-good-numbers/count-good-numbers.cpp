f21f96e203e1b6a839861c9597789a7377fe4fe8
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-04-13 15:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.99%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     #define ll long long
-    const int M = 1e9+7;
-    ll pow(ll x, ll n){
-        if(n == 0) return 1ll;
-        ll temp = pow(x,n/2)%M;
-        temp = (temp * temp) % M;
-        if(n&1) return (temp * x % M) % M;
+    int M = 1e9+7;
+    ll pow(ll x, ll y){
+        if(y == 0) return 1;
+        ll temp = pow(x, y/2)%M;
+        temp = (temp*temp)%M;
+        if(y&1) return (x*temp)%M;
         return temp;
     }
     int countGoodNumbers(long long n) {
         ll odd = n/2, even = (n+1)/2;
-        ll a = pow(5,even)%M;
-        ll b = pow(4,odd)%M;
-        return (int)((a*b)%M);
+        ll a = pow(4,odd)%M;
+        ll b = pow(5,even)%M;
+        return (a*b)%M;
     }
 };
