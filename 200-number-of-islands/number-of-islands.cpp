f62a5a0a4fb0295d1624ba74c31baea66d3f4f8b
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-04-17 22:11:49 +0530

    Time: 20 ms (93.96%) | Memory: 17.4 MB (51.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>&grid, vector<vector<bool>>&vis, int &n, int &m, int i, int j){
-        if(i>=n || j>=m || i<0 || j<0 || vis[i][j] || grid[i][j] == '0') return;
+    int dr[4] = {0,0,-1,1};
+    int dc[4] = {-1,1,0,0};
+    void solve(int i, int j, vector<vector<int>>&vis, vector<vector<char>>&grid){
+        int n = grid.size(), m = grid[0].size();
+        if(i>=n || i<0 || j<0 || j>=m || grid[i][j] == '0' || vis[i][j]) return;
         vis[i][j] = 1;
-        solve(grid,vis,n,m,i+1,j);
-        solve(grid,vis,n,m,i-1,j);
-        solve(grid,vis,n,m,i,j+1);
-        solve(grid,vis,n,m,i,j-1);
+        for(int k = 0; k<4; k++){
+            solve(i+dr[k], j+dc[k], vis,grid);
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0, n = grid.size(), m = grid[0].size();
-        vector<vector<bool>>vis(n, vector<bool>(m,0));
+        int n = grid.size(), m = grid[0].size(), cnt = 0;
+        vector<vector<int>>vis(n, vector<int>(m, 0));
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(vis[i][j] || grid[i][j] == '0') continue;
-                solve(grid,vis,n,m,i,j);
-                ans++;
+                solve(i,j,vis, grid);
+                cnt++;
             }
         }
-        return ans;
+        return cnt;
     }
 };
