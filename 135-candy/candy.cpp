3ce327a012d72c5604bce8872ef7862fe28f4071
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-06-02 20:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.50%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n = ratings.size();
+    int candy(vector<int>& nums) {
+        int n = nums.size();
         vector<int>ans(n,1);
         for(int i = 0; i<n-1; i++){
-            if(ratings[i]<ratings[i+1]) ans[i+1] = 1+ans[i];
+            if(nums[i]<nums[i+1]) ans[i+1] = 1+ans[i];
         }
         for(int i = n-1; i>0; i--){
-            if(ratings[i]<ratings[i-1]) ans[i-1] = max(ans[i-1], ans[i]+1);
+            if(nums[i]<nums[i-1]) ans[i-1] = max(1+ans[i], ans[i-1]);
         }
-        int cnt = accumulate(ans.begin(), ans.end(), 0);
-        return cnt;
+        // for(int i : ans) cout<<i<<" ";
+        return accumulate(ans.begin(), ans.end(),0);
     }
 };
