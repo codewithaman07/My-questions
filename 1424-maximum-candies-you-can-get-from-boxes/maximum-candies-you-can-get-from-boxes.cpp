6467caa0df9a35f1b0ba235ad928f48b763f7ee2
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-06-04 18:02:39 +0530

    Time: 9 ms (32.72%) | Memory: 45.2 MB (20.28%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& vis, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        bool foundOpenable = true;
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys,
+                   vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        
+        int n = status.size();
+        vector<bool> used(n, false);        
+        vector<bool> hasBox(n, false);      
+        vector<bool> hasKey(n, false);      
+        
+        queue<int> q;
+        
+        for (int box : initialBoxes) {
+            hasBox[box] = true;
+            q.push(box);
+        }
         int totalCandies = 0;
-        while (!initialBoxes.empty() && foundOpenable) {
-            foundOpenable = false;
-            vector<int> nextBoxes;
-            for (int boxId : initialBoxes) {
-                if (vis[boxId]) {
-                    foundOpenable = true;
-                    nextBoxes.insert(end(nextBoxes), begin(containedBoxes[boxId]), end(containedBoxes[boxId]));
-                    for (int keyId : keys[boxId]) vis[keyId] = 1;
-                    totalCandies += candies[boxId];
-                } else {
-                    nextBoxes.push_back(boxId);
+        while (!q.empty()) {
+            int box = q.front();
+            q.pop();
+            
+            if (!status[box] && !hasKey[box]) continue; 
+            if (used[box]) continue;                    
+            used[box] = true;
+            totalCandies += candies[box];
+            for (int key : keys[box]) {
+                hasKey[key] = true;
+                if (hasBox[key] && !used[key]) {
+                    q.push(key);
                 }
             }
-            swap(initialBoxes, nextBoxes);
-        }
+            for (int newBox : containedBoxes[box]) {
+                hasBox[newBox] = true;
+                if (!used[newBox]) {
+                    q.push(newBox);
+                }
+            }
+        }  
         return totalCandies;
     }
 };
