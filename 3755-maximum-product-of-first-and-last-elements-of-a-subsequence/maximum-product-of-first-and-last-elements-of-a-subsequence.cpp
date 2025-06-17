34f6554b5ae8c3f173133bb97bd45b0352c9ef92
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-06-17 10:16:11 +0530

    Time: 10 ms (68.03%) | Memory: 203.8 MB (29.00%) - LeetSync

--- 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
+++ 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
@@ -4,18 +4,13 @@ public:
     long long maximumProduct(vector<int>& nums, int m) {
         int n = nums.size();
         ll ans = LLONG_MIN;
-        vector<ll>mini(n,INT_MAX), maxi(n,INT_MIN);
+        vector<ll>mini(n,LLONG_MAX), maxi(n,LLONG_MIN);
         mini[n-1] = nums[n-1];
         maxi[n-1] = nums[n-1];
         for(int i = n-2; i>=0; i--){
             maxi[i] = max(nums[i]*1ll, maxi[i+1]);
             mini[i] = min(nums[i]*1ll, mini[i+1]);
         }
-        if(m == 1){
-            ans = max(maxi[0]*maxi[0], mini[0]*mini[0]);
-            return ans;
-        }
-        
         for(int i = 0; i<n-m+1; i++){
             ans = max(ans, nums[i]*maxi[i+m-1]);
             ans = max(ans, nums[i]*mini[i+m-1]);
