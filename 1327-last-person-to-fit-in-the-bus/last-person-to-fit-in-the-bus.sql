bb196590753bfbe4531a00a56985803a4107009f
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-07-13 16:23:09 +0530

    Time: 773 ms (72.11%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,6 +1,6 @@
 SELECT person_name
 FROM (
-  SELECT person_name, weight, turn, SUM(weight) OVER (ORDER BY turn) AS cum_wt
+  SELECT person_name, SUM(weight) OVER (ORDER BY turn) AS cum_wt
   FROM Queue
 ) AS sub
 WHERE cum_wt <= 1000
