cd641c4311bf139ca2e2e12c8c50564e6ed1707f
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-07-27 20:59:26 +0530

    Time: 19 ms (78.94%) | Memory: 14.1 MB (25.09%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -8,7 +8,6 @@ public:
         vector<vector<int>>dp(n, vector<int>(n,0));
         for(int l = n-2; l>=1; l--){
             for(int r = l; r<=n-2; r++){
-                if(l>r) continue;
                 int mini = INT_MAX;
                 for(int i = l; i<=r; i++){
                     int ans = dp[l][i-1]+dp[i+1][r]+(cuts[r+1]-cuts[l-1]);
