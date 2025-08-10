a26daf655a9cc9a242bdf36d29b1a4a54ea16532
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-08-10 23:59:28 +0530

    Time: 122 ms (1.43%) | Memory: 8.4 MB (33.76%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool pal(string &s){
+        int l = 0, r = s.size()-1;
+        while(l<r){
+            if(s[l++] != s[r--]) return 0;
+        }
+        return 1;
+    }
+    string breakPalindrome(string s) {
+        int n = s.size();
+        if(n == 1) return "";
+        string best;
+        for(int i = 0; i<n; i++){
+            char temp = s[i];
+            for(char ch = 'a'; ch<='z'; ch++){
+                if(ch == temp) continue;
+                s[i] = ch;
+                if(!pal(s)){
+                    if(best.empty() || s < best) best = s;
+                }
+            }
+            s[i] = temp;
+        }
+        return best;
+    }
+};
