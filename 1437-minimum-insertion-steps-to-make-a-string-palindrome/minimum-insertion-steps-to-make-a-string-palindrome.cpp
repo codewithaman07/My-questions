7b30dbfcdaff19d9be9aaaea83a15f7728353bb8
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-08-22 21:59:11 +0530

    Time: 67 ms (6.95%) | Memory: 31.6 MB (37.93%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, string &t, vector<vector<int>>&dp){
+    int solve(string &s, string &t, int i, int j, vector<vector<int>>&dp){
         if(i<0 || j<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]) return dp[i][j] = 1+solve(i-1,j-1,s,t,dp);
-        return dp[i][j] = max(solve(i-1,j,s,t,dp), solve(i,j-1,s,t,dp));
+        if(s[i] == t[j]) return dp[i][j] = 1+solve(s,t,i-1,j-1,dp);
+        else{
+            return dp[i][j] = max(solve(s,t,i-1,j,dp), solve(s,t,i,j-1,dp));
+        }
     }
     int minInsertions(string s) {
         int n = s.size();
         string t = s;
         reverse(t.begin(), t.end());
         vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return n-solve(n-1,n-1,s,t,dp);
+        return n-solve(s,t,n-1,n-1,dp);
     }
 };
