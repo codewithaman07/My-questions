54cf02b82f89ea0fd0237778a6f3c5d6c98fb20a
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-09-04 12:29:08 +0530

    Time: 1150 ms (32.31%) | Memory: 468.5 MB (35.17%) - LeetSync

---
+++ 3938-twisted-mirror-path-count/twisted-mirror-path-count.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    #define ll long long
+    int solve(int i, int j, vector<vector<int>>&grid, vector<vector<vector<int>>>&dp, int dir){
+        if(i<0 || j<0) return 0;
+        if(i == 0 && j == 0) return 1;
+        if(dp[i][j][dir] != -1) return dp[i][j][dir];
+        if(grid[i][j]){
+            if(dir == 1) return dp[i][j][dir] = solve(i-1,j,grid,dp,0)%mod;
+            else return dp[i][j][dir] = solve(i,j-1,grid,dp,1)%mod;
+        }
+        ll left = solve(i-1,j,grid,dp,0)%mod;
+        ll up = solve(i,j-1,grid,dp,1)%mod;
+        return dp[i][j][dir] = (left+up)%mod;
+    }
+    int uniquePaths(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(m, vector<int>(3,-1)));
+        return solve(n-1,m-1,grid,dp, 2)%mod;;
+    }
+};
