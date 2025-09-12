bb0a1c54c8fe5fef067cb339fe58193688f04b52
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-09-12 13:15:49 +0530

    Time: 8 ms (52.24%) | Memory: 19 MB (79.48%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int n = s.size(), cnt = 0;
+        for(int i = 0; i<n; i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+                cnt++;
+            }
+        }
+        if(cnt == 0) return false;
+        else return true;
+    }
+};
