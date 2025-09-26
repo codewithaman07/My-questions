3ceb2ae5e03018612f61e4f4309c21f49e1f46cd
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2025-09-26 12:19:35 +0530

    Time: 1 ms (44.22%) | Memory: 9.9 MB (9.75%) - LeetSync

--- 763-special-binary-string/special-binary-string.cpp
+++ 763-special-binary-string/special-binary-string.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     string makeLargestSpecial(string s) {
-        int cnt = 0, i = 0;
+        int cnt = 0, i = 0, n = s.size();
         vector<string>ans;
-        for(int j = 0; j<s.size(); j++){
+        for(int j = 0; j<n; j++){
             if(s[j] == '1') cnt++;
             else cnt--;
-            if(cnt == 0){
-                ans.push_back('1'+ makeLargestSpecial(s.substr(i+1, j-i-1)) + '0');
+            if(!cnt){
+                ans.push_back('1'+ makeLargestSpecial(s.substr(i+1, j-i-1)) +'0');
                 i = j+1;
             }
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.rbegin(), ans.rend());
         string t;
-        for(int i = ans.size()-1; i>=0; i--){
-            t+=ans[i];
-        }
+        for(auto v : ans) t+=v;
         return t;
     }
 };
